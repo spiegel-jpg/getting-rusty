8de2f18c7aadc00644b2ba0b4aaeb2a695b09944
Darpan Shrivastava <86070515+spiegel-jpg@users.noreply.github.com>
2023-01-19 08:12:28 -0500

    Delete 2/hello_cargo directory

--- 2/hello_cargo/target/debug/deps/hello_cargo-454b51d7a611d0ab.d
+++
@@ -1,5 +0,0 @@
-/Users/darpan/Desktop/learnrust/2/hello_cargo/target/debug/deps/hello_cargo-454b51d7a611d0ab.rmeta: src/main.rs
-
-/Users/darpan/Desktop/learnrust/2/hello_cargo/target/debug/deps/hello_cargo-454b51d7a611d0ab.d: src/main.rs
-
-src/main.rs:
--- 2/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/darpan/Desktop/learnrust/2/hello_cargo/target/debug/hello_cargo: /Users/darpan/Desktop/learnrust/2/hello_cargo/src/main.rs
